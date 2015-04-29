b159d9113f29fb4970079a3bf25388450c1dccb0
kenice washington <kenicek56@gmail.com>
2015-04-29 12:07:25 -0700

    finished my test

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Week Two Assessment</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+<div id="title" class="A col-x-12">
+	<h1>
+		Give Me an A!
+	</h1>
+</div>
+<div class="dan">
+	
+	<button type="button">Click me!</button>
+</div>
+<div id="iwao">
+
+<button type="button">Click me!</button>	
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="main.js"></script>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('.dan').click(function(){
+
+	alert("hey you clicked me!");
+
+	});
+
+});
+
+$(document).ready(function(){
+	$('#iwao').click(function(){
+	$('#iwao').hide();
+
+	});
+
+});
